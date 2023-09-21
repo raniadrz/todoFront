e2d2f5786a63750e6e5a501ce9c2d86d5a16bb3b
Rania <rania@Ranias-MacBook-Pro.local>
2023-09-21 19:15:37 +0300

    fixes side nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import BarChartIcon from '@mui/icons-material/BarChart';
 import ChecklistRtlIcon from "@mui/icons-material/ChecklistRtl";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import MenuIcon from '@mui/icons-material/Menu';
 import PasswordIcon from '@mui/icons-material/Password';
 import TimerIcon from '@mui/icons-material/Timer';
@@ -14,12 +15,11 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Toolbar from "@mui/material/Toolbar";
-import React from "react";
-import {
-  Link,
-  Route,
-  Routes
-} from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import React, { useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
+//paths
 import PasswordGenerator from "./components/PasswordGenerator";
 import TodoList1 from "./components/SimpleTodo";
 import TodoList2 from "./components/TodoDataG";
@@ -27,19 +27,23 @@ import Data from './components/data';
 import Time from "./components/timer";
 import logo from "./photos/logo.png";
 import "./styles/App.scss";
+
 const App = () => {
-  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
   const handleDrawerToggle = () => {
     setIsDrawerOpen(!isDrawerOpen);
   };
+
   const drawer = (
     <div className="drawer">
-      <div sx={{ display: "flex", justifyContent: "flex-end"}}>
+      <div sx={{ display: "flex", justifyContent: "flex-end" }}>
         <IconButton
           onClick={handleDrawerToggle}
-          sx={{ ml: 1 }}
+          sx={{ ml: 22 }}
           aria-label="close drawer"
         >
+          <ChevronRightIcon />
         </IconButton>
       </div>
       <div className="logoAppBar">
@@ -49,125 +53,79 @@ const App = () => {
           src={logo}
         />
       </div>
-      <Toolbar />
-      <List >
-        {/* TODO ONE HERE*/}
+    
+      <List>
         <ListItem component={Link} to="/list1">
-        <ListItemButton>
-          <ListItemIcon className='icons'>
-            <ChecklistRtlIcon />
-          </ListItemIcon>
-          <ListItemText primary="Todo List One"className="hide-on-mobile" />
-        </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon className='icons'>
+              <ChecklistRtlIcon />
+            </ListItemIcon>
+            <ListItemText primary="Todo List One" className="hide-on-mobile" />
+          </ListItemButton>
         </ListItem>
 
-        {/* TODO TWO HERE*/}
         <ListItem component={Link} to="/list2">
-        <ListItemButton>
-          <ListItemIcon className='icons'>
-            <ChecklistRtlIcon />
-          </ListItemIcon>
-          <ListItemText primary="Todo List Two" className="hide-on-mobile"/>
-        </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon className='icons'>
+              <ChecklistRtlIcon />
+            </ListItemIcon>
+            <ListItemText primary="Todo List Two" className="hide-on-mobile" />
+          </ListItemButton>
         </ListItem>
         <Divider />
 
-        {/* TIMER HERE*/}
         <ListItem component={Link} to="/Time">
-        <ListItemButton>
-          <ListItemIcon className='icons'>
-            <TimerIcon />
-          </ListItemIcon>
-          <ListItemText primary="Timer" className="hide-on-mobile"/>
-        </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon className='icons'>
+              <TimerIcon />
+            </ListItemIcon>
+            <ListItemText primary="Timer" className="hide-on-mobile" />
+          </ListItemButton>
         </ListItem>
 
-         {/* Password HERE*/}
-         <ListItem component={Link} to="/PasswordGenerator">
-        <ListItemButton>
-          <ListItemIcon className='icons'>
-            <PasswordIcon />
-          </ListItemIcon>
-          <ListItemText primary="Generator" className="hide-on-mobile"/>
-        </ListItemButton>
+        <ListItem component={Link} to="/PasswordGenerator">
+          <ListItemButton>
+            <ListItemIcon className='icons'>
+              <PasswordIcon />
+            </ListItemIcon>
+            <ListItemText primary="Generator" className="hide-on-mobile" />
+          </ListItemButton>
         </ListItem>
 
-          {/* DATA HERE*/}
         <ListItem component={Link} to="/data">
-        <ListItemButton>
-          <ListItemIcon className='icons'>
-            <BarChartIcon />
-          </ListItemIcon>
-          <ListItemText primary="Data" className="hide-on-mobile"/>
-        </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon className='icons'>
+              <BarChartIcon />
+            </ListItemIcon>
+            <ListItemText primary="Data" className="hide-on-mobile" />
+          </ListItemButton>
         </ListItem>
       </List>
     </div>
-    
   );
+
   return (
     <div className={`App ${isDrawerOpen ? "drawer-open" : ""}`}>
-      <Box
-          className="main-content"
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-          }}
-        ></Box>
-       <Box sx={{ display: "flex" }}>
-
-        {/* Side Navigation Drawer */}
-        <Drawer
-          anchor="left"
-          open={isDrawerOpen}
-          variant="persistent"
-          PaperProps={{
-            style: {
-              backgroundColor: '#ACBCFF',
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+      <Box className="main-content" sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
+        <AppBar className="appBar" position="fixed" open={isDrawerOpen}>
           <Toolbar>
-          
-          <Box
-          className="main-content"
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              p: 1,
-              pl: 2,
-              pr: 3,
-            }}
-          >
-            <IconButton 
+            <IconButton
               color="inherit"
-              aria-label="toggle drawer"
+              aria-label="open drawer"
               onClick={handleDrawerToggle}
               edge="start"
+              sx={{ mr: 2, ...(isDrawerOpen && { display: 'none' }) }}
             >
-              <MenuIcon />
+              {isDrawerOpen ? <ChevronRightIcon /> : <MenuIcon />}
             </IconButton>
-            {/* <Typography>Menu</Typography> */}
-          </Box>
-        </Box>
+            <Typography variant="h6" noWrap component="div">
+              React App!
+            </Typography>
           </Toolbar>
-         </AppBar>
-        </Box>
+        </AppBar>
       </Box>
+
       <Box sx={{ display: "flex" }}>
-        {/* Side Navigation Drawer */}
         <Drawer
           anchor="left"
           open={isDrawerOpen}
@@ -175,17 +133,7 @@ const App = () => {
         >
           {drawer}
         </Drawer>
-        
-        {/* Content */}
-        <Box
-          className="main-content"
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-          }}
-        >
-          {/* Page Content */}
+        <Box className="main-content" sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
           <Routes>
             <Route path="/list1" element={<TodoList1 />} />
             <Route path="/list2" element={<TodoList2 />} />
